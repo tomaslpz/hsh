9c03ebfeffd0c625bf5ae942a9274f453e908569
marianoOminetti <mariano.ominetti@gmail.com>
2018-11-08 19:38:56 -0300

    merge

---
+++ app/helpers/admins_helper.rb
@@ -0,0 +1,10 @@
+module AdminsHelper
+
+  def gravatar_for(admin)
+    gravatar_id =
+      Digest::MD5::hexdigest("mhartle@example.com")
+    gravatar_url =
+      "https://secure.gravatar.com/avatar#{gravatar_id}"
+      image_tag(gravatar_url, alt:'Foto de avatar', class: "gravatar")
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,29 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
 	root 'static_pages#home'
 
+  get 'admins/new', to:
+  'admins#new'
+
+  get '/signup', to:
+  'admins#new'
+
+  post '/signup', to:
+  'admins#create'
+
+
+	  get '/login', to:
+	  'sessions#new'
+
+	  post '/login', to:
+	  'sessions#create'
+
+		post '/logout', to:
+	  'sessions#destroy'
+
+
+  resources :admins
   resources :residences
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
