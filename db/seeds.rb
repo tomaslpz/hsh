1f2cf1f45c35455f9a7069939f205bb6e7cdda4d
renkw13 <matias.pxa@gmail.com>
2018-11-13 15:37:23 -0300

    Seguridad

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Admin.find_or_create_by(codigo: 'admin183121');
+
 Residence.find_or_create_by(nombre:'Parque Bonito', desc:'Residencia de veraneo y a poca distancia de la playa, exelente para pasar en familia', provincia:'Buenos Aires', partido:'Villa Gesell', ciudad:'Villa Gesell', img_link:'https://media-cdn.tripadvisor.com/media/photo-s/0a/95/b6/64/residencia-parque-bonito.jpg', direccion:'Almeda 208')
 Residence.find_or_create_by(nombre:'La Florida', desc:'En La Florida encontrarás un alojamiento innovador, situado en el corazón de Villa Gesell a orillas del mar.', provincia:'Buenos Aires', partido:'Villa Gesell', ciudad:'Villa Gesell', img_link:'https://cdn.bestday.net/_lib/vimages/Villa-Gesell-Argentina/Hotels/Villa-Gesell-Spa-Resort-ex-Days-Inn-Villa-Gesell/Fachada_g.jpg', direccion:'Sandias 223')
 Residence.find_or_create_by(nombre:'Boutique', desc:'Lugar tranquilo para relajarse y disfrutar', provincia:'Buenos Aires', partido:'Mar del Plata', ciudad:'Mar del Plata', img_link:'https://www.roblescasascampos.com/res/Inmuebles/1650_1562963236_750x450.jpg', direccion:'Av Miranda 350')
