d85487f1248535f7751513d010598eaf4b7d6860
marianoOminetti <mariano.ominetti@gmail.com>
2018-11-17 19:13:12 -0300

    login User, modificaciones de los admins en los layouts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
-    admin = Admin.find_by(codigo: params[:session][:codigo])
+    if params[:session][:email] == ''
+      admin = Admin.find_by(codigo: params[:session][:password])
          if admin
-           log_in admin
+           log_in_admin admin
            redirect_to admin
         else
           flash.now[:danger] = 'Código inválido'
           render 'new'
         end
+      else
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+          log_in_user user
+          redirect_to user
+
+        else
+              flash.now[:danger] = 'Invalid email/password combination'
+              render 'new'
+            end
+          end
       end
 
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,14 @@
 module SessionsHelper
 
-  def log_in(admin)
+  def log_in_admin(admin)
 
     session[:admin_id] = admin.id
+
+  end
+
+  def log_in_user(user)
+
+    session[:user_id] = user.id
 
   end
 
@@ -14,13 +20,29 @@ module SessionsHelper
     end
   end
 
-  def logged_in?
+  def current_user
+    if session[:user_id]
+      @current_user ||=
+      User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logged_in_admin?
     !current_admin.nil?
   end
 
-  def log_out
-      session.delete(:admin_id)
-      @current_admin = nil
-    end
+  def logged_in_user?
+    !current_user.nil?
+  end
 
-end
+  def log_out
+      if !:admin_id.nil?
+        session.delete(:admin_id)
+        @current_admin = nil
+      end
+    else
+        session.delete(:user_id)
+        @current_user = nil
+      end
+      
+  end
