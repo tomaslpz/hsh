4eb5f705069b9903f27afeb82e6b1cf27a196d2b
marianoOminetti <mariano.ominetti@gmail.com>
2018-11-17 19:58:32 -0300

    Se pulieron detalles de login y el registrar

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -7,4 +7,8 @@ module AdminsHelper
       "https://secure.gravatar.com/avatar#{gravatar_id}"
       image_tag(gravatar_url, alt:'Foto de avatar', class: "gravatar")
     end
+
+ def admin_new_path
+   redirect_to '/admin/new'
+ end
 end
