5c250f6070b82b6e1fb7616ccc72e532710496f1
renkw13 <matias.pxa@gmail.com>
2018-11-29 15:28:35 -0300

    fixes

--- db/migrate/20181117180726_add_password_digest_to_users.rb
+++ db/migrate/20181117180726_add_password_digest_to_users.rb
@@ -1,5 +1,8 @@
 class AddPasswordDigestToUsers < ActiveRecord::Migration[5.2]
   def change
+	create_table :users do |t|
+		t.timestamps
+	end
     add_column :users, :password_digest, :string
   end
 end
