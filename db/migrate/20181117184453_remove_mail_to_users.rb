d490d9c027631887f216b532fed737cae7c8f04d
renkw13 <matias.pxa@gmail.com>
2018-11-29 15:31:49 -0300

    fixes

--- db/migrate/20181117184453_remove_mail_to_users.rb
+++ db/migrate/20181117184453_remove_mail_to_users.rb
@@ -1,6 +1,8 @@
 class RemoveMailToUsers < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :mail, :string
-    add_column :users, :email, :string
+	if column_exists? :users, :mail
+    	remove_column :users, :mail, :string
+	end
+	add_column :users, :email, :string
   end
 end
